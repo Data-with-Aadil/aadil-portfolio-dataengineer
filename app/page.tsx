015752d612b6508a8fdb8bc1fdbf9d534fb2a4e0
Data-with-Aadil <131682034+Data-with-Aadil@users.noreply.github.com>
2025-12-14 18:04:10 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -171,14 +171,16 @@ export default function Home() {
               Aadil Mansoori
             </h1>
 
-            <p className="text-xl sm:text-2xl mb-6">
+            <p
+              className={`text-xl sm:text-2xl mb-6 ${
                 isLight ? "text-[#E11D48]" : "text-blue-400"
               }`}
             >
               Senior Data Engineer
             </p>
-
-            <p className="text-xl sm:text-2xl mb-6">
+            
+            <p
+              className={`text-lg mb-8 ${
                 isLight ? "text-gray-600" : "text-gray-300"
               }`}
             >
